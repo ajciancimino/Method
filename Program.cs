27831d5f369e11d45faee6f3b6e60c6e994fe56e
Ayrton Ciancimino <ajciancimino@usf.edu>
2020-02-04 19:15:39 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string input = Console.ReadLine();
+            string name = input;
+        }
+    }
+}
