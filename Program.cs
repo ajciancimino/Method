576eafd3f85a287a491d655c6ee78f4561d73ac0
Ayrton Ciancimino <ajciancimino@usf.edu>
2020-02-07 21:27:34 -0500

    Update Commit

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace Method
     {
         static void Main(string[] args)
         {
+            //Ask user for input
             Console.WriteLine("What is your name?");
             string input = Console.ReadLine();
             string name = input;
+            //run printName Method
+            PrintName(name);
+
+        }
+        private static void PrintName(string name)
+        {
+            Console.WriteLine("Hello"+" "+name);
         }
     }
 }
